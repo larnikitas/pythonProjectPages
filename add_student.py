600e09461e4a9e66b9ba55ab20bedf926fed581f
larnikitas@gmail.com <6090762Mosxos!10>
2023-10-11 08:20:05 +0300

    second load

--- add_student.py
+++ add_student.py
@@ -2,6 +2,7 @@ import streamlit as st
 from students import Student, Classroom
 from datetime import datetime
 
+
 def app(my_class_room):
     st.subheader('Add Student')
     next_id = my_class_room.get_next_student_id()
@@ -18,13 +19,14 @@ def app(my_class_room):
         submitted = st.form_submit_button("Submit", use_container_width=True)
 
         # If form is submitted and ID does not exist, add student
-        if submitted: # and len(errors) == 0:
+        if submitted:  # and len(errors) == 0:
             # Validation
             num_errors = validate_entry(my_class_room, id, fname, lname)
             if num_errors == 0:
                 stu1 = Student(id, fname, lname, dateBirth, height, weight, gender)
                 my_class_room.add_student(stu1)
                 st.success(f"Student {fname} {lname} added successfully")
+
 
 def validate_entry(my_class_room, id, fname, lname):
     # Validation
